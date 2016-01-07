274154cb2c9142b508f3512eba02d4e23e923460
Ulric Qin <ulric.qin@gmail.com>
2016-01-07 16:43:02 +0800

    no

--- a.go
+++ a.go
@@ -1,10 +1,10 @@
-package main
+packag main
 
 func main() {
 	if life.is_sad() || code.has_bug() || me.no_girlfriend() || purse.flat() {
 		drink_tea()
 		god.say("The world is awesome")
 	}
-}
+}                                                                                                   // 码农茶坊
 
 func foo() {}
